a92b390574d9bc7ea78f5057f8f4d9d66545e802
mikekauffman <mike.a.kauffman@gmail.com>
2014-08-11 15:27:32 -0600

    Styled checkins list page

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -2,7 +2,7 @@ class CheckinsController < ApplicationController
   def index
     user = current_user
     checkin = Checkin.new
-    @checkins_list = checkin.get_checkins(user)
+    @checkins = checkin.get_checkins(user)
   end
 
   def map
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,16 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     @user = current_user
-    @auth_url = "https://foursquare.com/oauth2/authenticate?client_id=#{ENV['CLIENT_ID']}&response_type=code&redirect_uri=#{redirect_uri}"
   end
 
   private
-
-  def redirect_uri
-    uri = URI.parse(request.url)
-    uri.path = '/auth/foursquare/callback'
-    uri.query = nil
-    uri
-  end
 
 end
